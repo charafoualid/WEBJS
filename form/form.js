877a495111fd5da44462d30a57dfd1e318b88f98
Harlod14 <91196636+Harlod14@users.noreply.github.com>
2023-03-16 18:58:29 +0100

    values added:
    - type transport
    - labels
    - tijd in interval

--- form/form.js
+++ form/form.js
@@ -7,6 +7,35 @@
 class Form {
     rootElement;
     stepCounter = 0;
+    steps = [{
+            stepName: "Stap 1",
+            fields: [{ label: "Length", name: "length", type: "text", required: true }, { label: "Width", name: "width", type: "text", required: true }]
+        },
+        {
+            stepName: "Stap 2",
+            fields: [{
+                label: "Interval (in seconds)",
+                name: "interval",
+                type: "number",
+                required: true
+            }]
+        },
+        {
+            stepName: "Stap 3",
+            fields: [{
+                label: "Type",
+                name: "type",
+                type: "select",
+                options: [
+                    { name: "Cold Transport" },
+                    { name: "Breakable Transport" },
+                    { name: "General Transport" },
+                    { name: "Pallet Transport" }
+                ],
+                required: true
+            }]
+        }
+    ];
 
     constructor(rootElement, stepCounter) {
         this.rootElement = rootElement;
