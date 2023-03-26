8863ad0ffdd7d0d9c5fd04535e55a1d02208c6e3
Harlod14 <91196636+Harlod14@users.noreply.github.com>
2023-03-26 16:29:36 +0200

    added error handling
    - added method that checks gridsize
    - added method that displays an error if a filled value does not meet requirements

--- app/app.js
+++ app/app.js
@@ -1,10 +1,6 @@
 import { Grid } from './grid.js';
-import {TruckBuilder} from './truckbuilder.js';
+import { TruckBuilder } from './truckbuilder.js';
 
 let truckBuilderEl = document.getElementById('truckbuilder');
 let gridEl = document.getElementById('grid');
 let truckbuilder = new TruckBuilder(truckBuilderEl, gridEl);
-
-// let gridEl = document.getElementById('grid');
-// let grid = new Grid(gridEl);
-
--- app/grid.js
+++ app/grid.js
@@ -1,13 +1,24 @@
-export class Grid{
-
-    constructor(parentEl){
+export class Grid {
+    constructor(parentEl) {
         this.parentEl = parentEl;
+        this.grids = [];
     }
 
-    addButton(button){
-        let btn = document.createElement('button');
-        btn.innerText = "Test!";
-        this.parentEl.appendChild(btn);
+    generateGrid(width, height) {
+        const grid = document.createElement("table");
+        for (let i = 0; i < height; i++) {
+            const row = document.createElement("tr");
+            for (let j = 0; j < width; j++) {
+                const cell = document.createElement("td");
+                cell.setAttribute("data-x", j);
+                cell.setAttribute("data-y", i);
+                row.appendChild(cell);
+            }
+            grid.appendChild(row);
+        }
+        this.grids.push(grid);
+        this.parentEl.appendChild(grid);
     }
+
 
 }
