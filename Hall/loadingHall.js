c7b2ed82eb42b578b263c082b641ab5b32bc7ff2
Harlod14 <91196636+Harlod14@users.noreply.github.com>
2023-03-28 02:02:01 +0200

    added stone styling
    stone now visible on assemblyLine

--- Hall/loadingHall.js
+++ Hall/loadingHall.js
@@ -3,6 +3,8 @@ export class LoadingHall {
     loadingHallEl;
     assemblyLineContainer = document.createElement('div');
     assemblyLines = [3];
+    tetrominoStones = ['Images/t_Tetromino.png', 'Images/straigth_Tetromino.png', 'Images/square_Tetromino.png', 'Images/skew_Tetromino.png', 'Images/l_Tetromino.png'];
+
 
     constructor(addbutton, switchHall, loadingHallEl) {
         this.loadingHallEl = loadingHallEl;
@@ -18,8 +20,9 @@ export class LoadingHall {
             assembly.className = 'assembly';
             this.assemblyLines.push(assembly);
             this.assemblyLineContainer.appendChild(assembly);
-        }
+            this.addTetromino(assembly);
 
+        }
     }
     handleAddButton() {
         this.createAssembly();
@@ -28,6 +31,15 @@ export class LoadingHall {
     createAssemblyContainer() {
         this.assemblyLineContainer.className = 'assemblyContainer';
         this.loadingHallEl.appendChild(this.assemblyLineContainer);
+    }
+
+    addTetromino(assembly) {
+        const tetrominoIndex = Math.floor(Math.random() * this.tetrominoStones.length);
+        const tetrominoSrc = this.tetrominoStones[tetrominoIndex];
+        const tetrominoEl = document.createElement('div');
+        tetrominoEl.className = 'tetromino';
+        tetrominoEl.style.backgroundImage = `url(${tetrominoSrc})`;
+        assembly.appendChild(tetrominoEl);
     }
 
 
