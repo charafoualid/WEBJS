9218e4528f68fab8e97354e8cf09b54e33ea1eed
Harlod14 <91196636+Harlod14@users.noreply.github.com>
2023-03-28 02:42:35 +0200

    added animation
    - stones can move now
    - animation speed added

--- Hall/loadingHall.js
+++ Hall/loadingHall.js
@@ -5,7 +5,6 @@ export class LoadingHall {
     assemblyLines = [3];
     tetrominoStones = ['Images/t_Tetromino.png', 'Images/straigth_Tetromino.png', 'Images/square_Tetromino.png', 'Images/skew_Tetromino.png', 'Images/l_Tetromino.png'];
 
-
     constructor(addbutton, switchHall, loadingHallEl) {
         this.loadingHallEl = loadingHallEl;
         this.addbutton = addbutton;
@@ -21,9 +20,11 @@ export class LoadingHall {
             this.assemblyLines.push(assembly);
             this.assemblyLineContainer.appendChild(assembly);
             this.addTetromino(assembly);
-
+            this.movePackages(); // start the animation
         }
     }
+
+
     handleAddButton() {
         this.createAssembly();
     }
@@ -36,12 +37,49 @@ export class LoadingHall {
     addTetromino(assembly) {
         const tetrominoIndex = Math.floor(Math.random() * this.tetrominoStones.length);
         const tetrominoSrc = this.tetrominoStones[tetrominoIndex];
-        const tetrominoEl = document.createElement('div');
+        const tetrominoEl = document.createElement('img');
         tetrominoEl.className = 'tetromino';
-        tetrominoEl.style.backgroundImage = `url(${tetrominoSrc})`;
+        tetrominoEl.src = tetrominoSrc;
         assembly.appendChild(tetrominoEl);
     }
 
+    movePackages() {
+        const speed = 8; // set the speed of the animation
+        const assemblyLineWidth = 750; // set the width of the assembly line
+
+        // loop through each assembly line
+        this.assemblyLines.forEach((assembly, index) => {
+            // check if the assembly is a valid element
+            if (assembly instanceof Element) {
+                // get all the tetromino elements in the assembly line
+                const tetrominos = assembly.querySelectorAll('.tetromino');
+
+                // loop through each tetromino element
+                tetrominos.forEach((tetromino, tetrominoIndex) => {
+                    // set the animation properties
+                    tetromino.style.animation = `move ${speed}s linear`;
+                    tetromino.style.animationDelay = `${(index * tetrominos.length + tetrominoIndex) / 2}s`;
+                    tetromino.style.animationIterationCount = 'infinite';
+                    tetromino.style.animationName = 'move-stone';
+
+                    // create a style element with the keyframes
+                    const keyframes = `@keyframes move-stone {
+                    from {
+                        transform: translateX(0);
+                    }
+                    to {
+                        transform: translateX(${assemblyLineWidth}px);
+                    }
+                  }`;
+                    const styleEl = document.createElement('style');
+                    styleEl.textContent = keyframes;
+
+                    // add the style element to the document head
+                    document.head.appendChild(styleEl);
+                });
+            }
+        });
+    }
 
 
 }
