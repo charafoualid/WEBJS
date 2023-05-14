94e0573113d331925ba326de06ab47e4debc1544
Charaf Oualid <mc.oualid@student.avans.nl>
2023-05-14 15:15:04 +0200

    weather api update

--- app/weather_api.js
+++ app/weather_api.js
@@ -3,15 +3,13 @@
 function renderWeather(weather){
     console.log(weather);
     var resultsContainer = document.querySelector("#weather-results");
+
+    resultsContainer.innerHTML = '';
+    
     var city = document.createElement("h2");
     city.textContent = weather.name;
     resultsContainer.append(city);
-
-    var inputField = document.createElement("input");
-    inputField.placeholder = "city/location";
-    inputField.id = "myInputField";
-    resultsContainer.append(inputField);
-
+   
     var temp = document.createElement("p");
     temp.textContent = "Temp: " + weather.main.temp + " F";
     resultsContainer.append(temp);
@@ -31,24 +29,17 @@ function renderWeather(weather){
         resultsContainer.append(description);
     }
 
-    details.append("")
 }
 
-//Fetch weather data for city
-function fetchWeather(query){
 
-    let input = document.getElementById("myInputField");
-    let city = input.value;
-    console.log(city);
+function fetchWeather(){
+    let input = document.getElementById("myText").value;
+    console.log(input);
 
-    // var url = "https://api.openweathermap.org/data/2.5/weather?q=Tilburg&units=imperial&appid=2158b761cc7aecf9f34cade3f4fe213e";
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=2158b761cc7aecf9f34cade3f4fe213e";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=2158b761cc7aecf9f34cade3f4fe213e";
 
     fetch(url)
      .then((response) => response.json())
      .then((data) => renderWeather(data));
 }
 
-inputField.addEventListener("change", function() {
-    fetchWeather();
-});
